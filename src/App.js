0db41e0cecf42875fd1f95969d845a271cf54e22
MisterVerhoef <frankverhoef@outlook.com>
2022-05-30 18:15:58 +0200

    klaar voor controle

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import PokemonCard from "./component/PokemonCard";
-import pokemonLogo from "../src/assets/Pokemon-Logo.png"
+import React from 'react';
 import './App.css';
 import Homepage from "./pages/Homepage";
 
 
 
 function App() {
-
-    // const [pokemon, setPokemon] = useState([])
-    // const [endpoint, setEndpoint] = useState(`https://pokeapi.co/api/v2/pokemon`)
-    //
-    // useEffect(() => {
-    //     async function getPokemonData() {
-    //
-    //         try {
-    //             const result = await axios.get(`${endpoint}`);
-    //             console.log(result.data);
-    //             setPokemon(result.data);
-    //         } catch (e) {
-    //             console.error(e);
-    //         }
-    //     }
-    //     getPokemonData()
-    // }, [endpoint])
 
 
   return (
