4ebc0b83ec555c69d4092ce09f2313ca16d514b8
MisterVerhoef <frankverhoef@outlook.com>
2022-05-30 13:42:45 +0200

    klaar behalve wat styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import PokemonCard from "./component/PokemonCard";
+import pokemonLogo from "../src/assets/Pokemon-Logo.png"
 import './App.css';
+import Homepage from "./pages/Homepage";
+
+
 
 function App() {
+
+    // const [pokemon, setPokemon] = useState([])
+    // const [endpoint, setEndpoint] = useState(`https://pokeapi.co/api/v2/pokemon`)
+    //
+    // useEffect(() => {
+    //     async function getPokemonData() {
+    //
+    //         try {
+    //             const result = await axios.get(`${endpoint}`);
+    //             console.log(result.data);
+    //             setPokemon(result.data);
+    //         } catch (e) {
+    //             console.error(e);
+    //         }
+    //     }
+    //     getPokemonData()
+    // }, [endpoint])
+
+
   return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
+
+
+
+    <>
+        <Homepage/>
+    </>
+
   );
 }
 
